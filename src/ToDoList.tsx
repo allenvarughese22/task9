b7d48ab4fb783abbbf315df09bef5f163c788e8e
allenvarughese22 <allenvrghs2007@gmail.com>
2023-12-27 17:33:56 -0500

    css changed

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./TodoList.css";
 
 interface Task {
   id: number;
@@ -6,7 +7,7 @@ interface Task {
 }
 
 const TodoList = () => {
-  const [tasks, setTasks] = useState<Task[]>([]); // Provide a type annotation for tasks
+  const [tasks, setTasks] = useState<Task[]>([]);
   const [newTask, setNewTask] = useState("");
 
   const addTask = (e: React.FormEvent<HTMLFormElement>) => {
@@ -16,20 +17,37 @@ const TodoList = () => {
     setNewTask("");
   };
 
+  const deleteTask = (taskId: number) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div>
-      <form onSubmit={addTask}>
+    <div className="container">
+      <h1 className="title">Todo List</h1>
+      <form onSubmit={addTask} className="form-container">
         <input
           type="text"
           value={newTask}
           onChange={(e) => setNewTask(e.target.value)}
           placeholder="Add new task"
+          className="input-field"
         />
-        <button type="submit">Add</button>
+        <button type="submit" className="add-button">
+          Add
+        </button>
       </form>
-      <ul>
+      <ul className="task-list">
         {tasks.map((task) => (
-          <li key={task.id}>{task.title}</li>
+          <li key={task.id} className="task">
+            <span className="task-title">{task.title}</span>
+            <button
+              className="delete-button"
+              onClick={() => deleteTask(task.id)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
